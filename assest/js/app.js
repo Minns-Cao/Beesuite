9bfbd028b15f6099517b56ca3ddae41735b4a962
Minns-Cao <se182593caonguyenhoangminh@gmail.com>
2023-11-16 15:41:09 +0700

    mobile responsive

--- assest/js/app.js
+++ assest/js/app.js
@@ -16,6 +16,7 @@ try {
 try {
     var swiperfeedback = new Swiper(".feedback", {
         loop: true,
+       
         pagination: {
             el: ".feedbackPagination",
         },
@@ -29,9 +30,25 @@ try {
 }
 
 try {
-    var swiperfeedback = new Swiper(".storySilde", {
+    var swiperStorySlide = new Swiper(".storySilde", {
         slidesPerView: 3,
-        spaceBetween: 74,
+        spaceBetween: 74, 
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            640: {
+                slidesPerView: 1,
+    
+                spaceBetween: 20,
+            },
+            1200: {
+                slidesPerView: 2,
+    
+                spaceBetween: 20,
+            },
+        },
         loop: true,
         pagination: {
             el: ".storyPagination",
@@ -100,11 +117,11 @@ const REG_NAME =
 const isRequired = (value) => (value !== "" ? "" : "Thông tin cần điền");
 const isEmail = (value) =>
     REG_EMAIL.test(value) ? "" : "Vui lòng nhập đúng định dạng Email";
-const isName = (value) => (REG_NAME.test(value) ? "" : "Vui lòng nhập đúng định dạng tên");
+const isName = (value) =>
+    REG_NAME.test(value) ? "" : "Vui lòng nhập đúng định dạng tên";
 
 const isSame = (paramValue, fieldname1, fieldname2) => (value) =>
     paramValue === value ? "" : `${fieldname1} không khớp ${fieldname2}`;
-
 
 // value: giá trị của controlNode
 // Funcs : mảng các hàm mà value cần check
@@ -137,7 +154,7 @@ const isValid = (paramObject) => {
 const showNoti = () => {
     const notiBox = document.querySelector(".notiBox");
     notiBox.classList.add("show");
-}
+};
 
 // hàm xoá hết các thông báo lỗi trên UI
 const clearMsg = () => {
@@ -149,55 +166,71 @@ const clearMsg = () => {
     });
 };
 
-document.querySelector(".formContact").addEventListener("submit", (event) => {
-    event.preventDefault(); // chặn reset trang
-    clearMsg(); // xoá các báo lỗi trước đó
-    let emailNode = document.querySelector("#email");
-    let nameNode = document.querySelector("#name");
-    let companyNode = document.querySelector("#company");
-    let countryNode = document.querySelector("input[name='country']:checked");
-    let phoneNode = document.querySelector("#phone");
-    let confirmedPasswordNode = document.querySelector("#confirmedPassword");
-    let confirm1Node = document.querySelector("input#confirm1:checked");
-    const erroeMsg = [
-        //Email
-        isValid({
-            value: emailNode.value,
-            funcs: [isRequired, isEmail],
-            parentNode: emailNode.parentElement,
-            controlNodes: [emailNode],
-        }),
-        //Name
-        isValid({
-            value: nameNode.value,
-            funcs: [isRequired, isName],
-            parentNode: nameNode.parentElement,
-            controlNodes: [nameNode],
-        }),
-        isValid({
-            value: companyNode.value,
-            funcs: [isRequired],
-            parentNode: companyNode.parentElement,
-            controlNodes: [companyNode],
-        }),
-        isValid({
-            value: phoneNode.value,
-            funcs: [isRequired],
-            parentNode: phoneNode.parentElement,
-            controlNodes: [phoneNode],
-        }),
-        //agree
-        isValid({
-            value: confirm1Node ? confirm1Node.value : "",
-            funcs: [isRequired],
-            parentNode: document.querySelector("input#confirm1").parentElement,
-            controlNodes: [document.querySelector("input#confirm1")],
-        })
-        
-    ];
-    const isValidForm = erroeMsg.every((item) => !item);
-    if (isValidForm) {
-        clearMsg();
-        showNoti();
-    }
-});
+let mbNav = document.querySelector(".mbNav");
+let iconNavMb = document.querySelector(".iconNavMb").addEventListener("click",event => {
+    mbNav.classList.add("active");
+})
+let btnClose = document.querySelector(".btnClose").addEventListener("click",event => {
+    mbNav.classList.remove("active");
+})
+
+
+if (document.querySelector(".formContact")) {
+    document
+        .querySelector(".formContact")
+        .addEventListener("submit", (event) => {
+            event.preventDefault(); // chặn reset trang
+            clearMsg(); // xoá các báo lỗi trước đó
+            let emailNode = document.querySelector("#email");
+            let nameNode = document.querySelector("#name");
+            let companyNode = document.querySelector("#company");
+            let countryNode = document.querySelector(
+                "input[name='country']:checked"
+            );
+            let phoneNode = document.querySelector("#phone");
+            let confirmedPasswordNode =
+                document.querySelector("#confirmedPassword");
+            let confirm1Node = document.querySelector("input#confirm1:checked");
+            const erroeMsg = [
+                //Email
+                isValid({
+                    value: emailNode.value,
+                    funcs: [isRequired, isEmail],
+                    parentNode: emailNode.parentElement,
+                    controlNodes: [emailNode],
+                }),
+                //Name
+                isValid({
+                    value: nameNode.value,
+                    funcs: [isRequired, isName],
+                    parentNode: nameNode.parentElement,
+                    controlNodes: [nameNode],
+                }),
+                isValid({
+                    value: companyNode.value,
+                    funcs: [isRequired],
+                    parentNode: companyNode.parentElement,
+                    controlNodes: [companyNode],
+                }),
+                isValid({
+                    value: phoneNode.value,
+                    funcs: [isRequired],
+                    parentNode: phoneNode.parentElement,
+                    controlNodes: [phoneNode],
+                }),
+                //agree
+                isValid({
+                    value: confirm1Node ? confirm1Node.value : "",
+                    funcs: [isRequired],
+                    parentNode:
+                        document.querySelector("input#confirm1").parentElement,
+                    controlNodes: [document.querySelector("input#confirm1")],
+                }),
+            ];
+            const isValidForm = erroeMsg.every((item) => !item);
+            if (isValidForm) {
+                clearMsg();
+                showNoti();
+            }
+        });
+}
